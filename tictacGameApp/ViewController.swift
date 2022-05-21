47e4ad25ae988e528b59476f28363c6b5517d7c1
c0850919 <c0850919@mylambton.ca>
2022-05-21 00:45:41 -0400

    Connecting buttons from view to view controller

--- tictacGameApp/ViewController.swift
+++ tictacGameApp/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    
+    //linking the lable to the view controller
+    @IBOutlet weak var showCurrentTurn: UILabel!
+    
+    //linking the buttons
+    @IBOutlet weak var first: UIButton!
+    @IBOutlet weak var second: UIButton!
+    @IBOutlet weak var third: UIButton!
+    @IBOutlet weak var forth: UIButton!
+    @IBOutlet weak var fifth: UIButton!
+    @IBOutlet weak var sixth: UIButton!
+    @IBOutlet weak var seventh: UIButton!
+    @IBOutlet weak var eighth: UIButton!
+    @IBOutlet weak var ninth: UIButton!
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+                
     }
 
-
+    
+    //creating action that will respones when user will touch any of the button
+    
+    @IBAction func tapAction(_ sender: UIButton) {
+    }
+    
 }
 
