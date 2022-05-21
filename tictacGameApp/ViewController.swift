bc75386a71175d9c434db4af36cf9c40200b5288
c0850919 <c0850919@mylambton.ca>
2022-05-21 00:05:25 -0400

    Initial Commit

---
+++ tictacGameApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tictacGameApp
+//
+//  Created by user219793 on 5/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
